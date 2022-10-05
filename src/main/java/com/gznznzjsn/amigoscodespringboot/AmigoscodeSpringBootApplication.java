e8f88d7b30db6e27a0a43d6282ae72edb058267a
gznznzjsn <arik2046@gmail.com>
2022-10-05 14:04:52 +0300

    created hello world app

---
+++ src/main/java/com/gznznzjsn/amigoscodespringboot/AmigoscodeSpringBootApplication.java
@@ -0,0 +1,22 @@
+package com.gznznzjsn.amigoscodespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class AmigoscodeSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmigoscodeSpringBootApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public List<String> hello() {
+        return List.of("Hello", "World");
+    }
+}
---
+++ src/test/java/com/gznznzjsn/amigoscodespringboot/AmigoscodeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gznznzjsn.amigoscodespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
