50444080837415b3c4eaadb5b0661160b7f06567
gznznzjsn <arik2046@gmail.com>
2022-11-20 16:44:16 +0300

    Project init

---
+++ src/main/java/com/gznznzjsn/amigoscodeloginandregistration/AmigoscodeLoginAndRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.gznznzjsn.amigoscodeloginandregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodeLoginAndRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoscodeLoginAndRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gznznzjsn/amigoscodeloginandregistration/AmigoscodeLoginAndRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gznznzjsn.amigoscodeloginandregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodeLoginAndRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
